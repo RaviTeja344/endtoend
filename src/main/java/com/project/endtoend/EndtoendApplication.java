20355f6396a56b9357fa5c94592e81354eb529bf
Ravi Teja Polisetty <raviteja@Ravis-MacBook-Pro.local>
2023-09-15 15:58:05 -0400

    Changes

--- src/main/java/com/project/endtoend/EndtoendApplication.java
+++ src/main/java/com/project/endtoend/EndtoendApplication.java
@@ -8,6 +8,8 @@ public class EndtoendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EndtoendApplication.class, args);
+
+		System.out.println("Hello WORLD");
 	}
 
 }
